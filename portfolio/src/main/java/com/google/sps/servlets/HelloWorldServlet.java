29fc47b1911adda5bf34453a9c88b7bd3759fd8f
Tommy Liang <tliang@sps-program.com>
2022-06-02 03:21:34 +0000

    Follow tutorial for /hello extension on webpage

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -10,9 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+    private String first_name = "Tommy";
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;");
+        response.getWriter().println("<h1>Hello " + first_name + "!</h1>");
+    }
 }
